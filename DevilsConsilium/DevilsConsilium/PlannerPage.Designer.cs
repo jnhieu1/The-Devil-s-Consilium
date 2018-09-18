7bab4e8388fdccf1a8482ef88254720728580f6f
Charlie Howard <choward13@brophybroncos.org>
2018-09-18 16:08:35 -0700

    Commiting most current project

--- DevilsConsilium/DevilsConsilium/PlannerPage.Designer.cs
+++ DevilsConsilium/DevilsConsilium/PlannerPage.Designer.cs
@@ -33,11 +33,35 @@
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.courseGroupBox = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.year1GroupBox = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.year2GroupBox = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
             this.year3GroupBox = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
             this.year4GroupBox = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox8 = new System.Windows.Forms.ListBox();
+            this.listBox7 = new System.Windows.Forms.ListBox();
+            this.reportGroupBox = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
+            this.courseGroupBox.SuspendLayout();
+            this.year1GroupBox.SuspendLayout();
+            this.year2GroupBox.SuspendLayout();
+            this.year3GroupBox.SuspendLayout();
+            this.year4GroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -72,15 +96,29 @@
             // 
             // courseGroupBox
             // 
+            this.courseGroupBox.Controls.Add(this.button1);
+            this.courseGroupBox.Controls.Add(this.textBox1);
             this.courseGroupBox.Location = new System.Drawing.Point(12, 27);
             this.courseGroupBox.Name = "courseGroupBox";
-            this.courseGroupBox.Size = new System.Drawing.Size(128, 311);
+            this.courseGroupBox.Size = new System.Drawing.Size(128, 326);
             this.courseGroupBox.TabIndex = 1;
             this.courseGroupBox.TabStop = false;
             this.courseGroupBox.Text = "Courses";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(115, 20);
+            this.textBox1.TabIndex = 0;
+            // 
             // year1GroupBox
             // 
+            this.year1GroupBox.Controls.Add(this.label6);
+            this.year1GroupBox.Controls.Add(this.label2);
+            this.year1GroupBox.Controls.Add(this.listBox2);
+            this.year1GroupBox.Controls.Add(this.listBox1);
             this.year1GroupBox.Location = new System.Drawing.Point(173, 27);
             this.year1GroupBox.Name = "year1GroupBox";
             this.year1GroupBox.Size = new System.Drawing.Size(499, 152);
@@ -88,8 +126,46 @@
             this.year1GroupBox.TabStop = false;
             this.year1GroupBox.Text = "Year 1 (Freshman)";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(252, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Semester 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Semester 1";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(255, 38);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(219, 108);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(20, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(219, 108);
+            this.listBox1.TabIndex = 0;
+            // 
             // year2GroupBox
             // 
+            this.year2GroupBox.Controls.Add(this.label7);
+            this.year2GroupBox.Controls.Add(this.label3);
+            this.year2GroupBox.Controls.Add(this.listBox4);
+            this.year2GroupBox.Controls.Add(this.listBox3);
             this.year2GroupBox.Location = new System.Drawing.Point(173, 186);
             this.year2GroupBox.Name = "year2GroupBox";
             this.year2GroupBox.Size = new System.Drawing.Size(499, 152);
@@ -97,8 +173,46 @@
             this.year2GroupBox.TabStop = false;
             this.year2GroupBox.Text = "Year 2 (Sophomore)";
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(252, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Semester 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Semester 1";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(255, 38);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(219, 108);
+            this.listBox4.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(20, 38);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(219, 108);
+            this.listBox3.TabIndex = 2;
+            // 
             // year3GroupBox
             // 
+            this.year3GroupBox.Controls.Add(this.label8);
+            this.year3GroupBox.Controls.Add(this.label4);
+            this.year3GroupBox.Controls.Add(this.listBox6);
+            this.year3GroupBox.Controls.Add(this.listBox5);
             this.year3GroupBox.Location = new System.Drawing.Point(173, 344);
             this.year3GroupBox.Name = "year3GroupBox";
             this.year3GroupBox.Size = new System.Drawing.Size(499, 152);
@@ -106,8 +220,46 @@
             this.year3GroupBox.TabStop = false;
             this.year3GroupBox.Text = "Year 3 (Junior)";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(252, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Semester 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Semester 1";
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(255, 38);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(219, 108);
+            this.listBox6.TabIndex = 5;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(20, 38);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(219, 108);
+            this.listBox5.TabIndex = 4;
+            // 
             // year4GroupBox
             // 
+            this.year4GroupBox.Controls.Add(this.label9);
+            this.year4GroupBox.Controls.Add(this.label5);
+            this.year4GroupBox.Controls.Add(this.listBox8);
+            this.year4GroupBox.Controls.Add(this.listBox7);
             this.year4GroupBox.Location = new System.Drawing.Point(173, 502);
             this.year4GroupBox.Name = "year4GroupBox";
             this.year4GroupBox.Size = new System.Drawing.Size(499, 152);
@@ -115,11 +267,64 @@
             this.year4GroupBox.TabStop = false;
             this.year4GroupBox.Text = "Year 4 (Senior)";
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(252, 22);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 13);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Semester 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Semester 1";
+            // 
+            // listBox8
+            // 
+            this.listBox8.FormattingEnabled = true;
+            this.listBox8.Location = new System.Drawing.Point(255, 38);
+            this.listBox8.Name = "listBox8";
+            this.listBox8.Size = new System.Drawing.Size(219, 108);
+            this.listBox8.TabIndex = 7;
+            // 
+            // listBox7
+            // 
+            this.listBox7.FormattingEnabled = true;
+            this.listBox7.Location = new System.Drawing.Point(20, 38);
+            this.listBox7.Name = "listBox7";
+            this.listBox7.Size = new System.Drawing.Size(219, 108);
+            this.listBox7.TabIndex = 6;
+            // 
+            // reportGroupBox
+            // 
+            this.reportGroupBox.Location = new System.Drawing.Point(13, 366);
+            this.reportGroupBox.Name = "reportGroupBox";
+            this.reportGroupBox.Size = new System.Drawing.Size(127, 271);
+            this.reportGroupBox.TabIndex = 4;
+            this.reportGroupBox.TabStop = false;
+            this.reportGroupBox.Text = "Report";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // PlannerPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 662);
+            this.Controls.Add(this.reportGroupBox);
             this.Controls.Add(this.year4GroupBox);
             this.Controls.Add(this.year3GroupBox);
             this.Controls.Add(this.year2GroupBox);
@@ -131,6 +336,16 @@
             this.Text = "Devil\'s Consilium";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.courseGroupBox.ResumeLayout(false);
+            this.courseGroupBox.PerformLayout();
+            this.year1GroupBox.ResumeLayout(false);
+            this.year1GroupBox.PerformLayout();
+            this.year2GroupBox.ResumeLayout(false);
+            this.year2GroupBox.PerformLayout();
+            this.year3GroupBox.ResumeLayout(false);
+            this.year3GroupBox.PerformLayout();
+            this.year4GroupBox.ResumeLayout(false);
+            this.year4GroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,6 +362,25 @@
         private System.Windows.Forms.GroupBox year2GroupBox;
         private System.Windows.Forms.GroupBox year3GroupBox;
         private System.Windows.Forms.GroupBox year4GroupBox;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox8;
+        private System.Windows.Forms.ListBox listBox7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox reportGroupBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
